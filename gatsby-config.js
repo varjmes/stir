9213411af9b7f98430b475a68350dc1627183c85
James Spencer <hello@jmes.tech>
2019-03-12 15:20:54 +0000

    feat: sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,16 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://stir.dev`,
     title: 'Stir',
     description:
-      'Learning to cook delicious meals, one step at a time. Experimenting with tried and true recipes and defining our own',
-    author: 'James Spencer',
+      `Learning to cook delicious meals, one step at a time. Experimenting with tried and true recipes and defining our own`,
+    author: `James Spencer`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
